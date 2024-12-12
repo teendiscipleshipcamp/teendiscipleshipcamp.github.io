fe7787f9c028eedc9d46e6d6196bac8fcb303eeb
fletchthefletch <fletcher@4tradeit.co.nz>
2024-12-12 18:16:13 +1300

    chore: added key

--- src/react-components/ContactForm.tsx
+++ src/react-components/ContactForm.tsx
@@ -20,9 +20,9 @@ const props = {
 
 
 const ContactFormInternal: FC = () => {
-	const EMAILJS_SERVICE_ID = process.env.EMAILJS_SERVICE_ID;// 'service_xa3xje9';
-	const EMAILJS_TEMPLATE_ID = process.env.EMAILJS_TEMPLATE_ID;//'template_8zbzu4t';
-	const EMAILJS_PUBLIC_KEY = process.env.EMAILJS_PUBLIC_KEY;//'2n9paRn-soyQjDywu';
+	const EMAILJS_SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;// 'service_xa3xje9';
+	const EMAILJS_TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;//'template_8zbzu4t';
+	const EMAILJS_PUBLIC_KEY = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;//'2n9paRn-soyQjDywu';
 	const form = useRef<HTMLFormElement | null>(null);
   
 	const sendEmail = (e) => {
