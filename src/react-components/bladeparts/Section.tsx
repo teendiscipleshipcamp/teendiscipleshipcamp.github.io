8b0f8dff19b550c32991f9497d7cd6852eef26eb
fletchthefletch <fletcher@4tradeit.co.nz>
2024-12-20 20:09:58 +1300

    chore: updated animations

--- src/react-components/bladeparts/Section.tsx
+++ src/react-components/bladeparts/Section.tsx
@@ -1,6 +1,7 @@
 import { observer } from "mobx-react-lite";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { AnimationSection } from "./AnimationSection.tsx";
+import cn from "classnames";
 
 interface Props {
 	id: string;
@@ -11,27 +12,42 @@ interface Props {
 	linkText: string;
 }
 
-const SectionInternal: FC<Props> = ({ id, imageSrc, title, description, link, linkText }) => (
-	<section id={id}>
-		<div className='image'>
-			<img src={imageSrc} alt='' data-position='top center' />
-		</div>
-		<div className='content'>
-			<AnimationSection animation='fade-in-section' className='inner'>
-				<header className='major'>
-					<h3>{title}</h3>
-				</header>
-				<p>{description}</p>
-				<ul className='actions'>
-					<li>
-						<a href={link} target="_blank" className='button' rel="noreferrer">
-							{linkText}
-						</a>
-					</li>
-				</ul>
-			</AnimationSection>
-		</div>
-	</section>
-);
+const SectionInternal: FC<Props> = ({ id, imageSrc, title, description, link, linkText }) => {
+	const [active, setActive] = useState(false);
+
+	const onHover = () => {
+		console.log('Hover entered');
+		setActive(true);
+
+	};
+
+	const onHoverExit = () => {
+		console.log('Hover left');
+		setActive(false);
+	};
+
+	return (
+		<section id={id}>
+			<div className={cn('image', { 'active': active })}>
+				<img src={imageSrc} alt='' data-position='top center' />
+			</div>
+			<div className='content'>
+				<AnimationSection animation='fade-in-section' className='inner'>
+					<header className='major'>
+						<h3>{title}</h3>
+					</header>
+					<p>{description}</p>
+					<ul className='actions'>
+						<li>
+							<a onMouseOver={onHover} onMouseOut={onHoverExit} href={link} target="_blank" className='button' rel="noreferrer">
+								{linkText}
+							</a>
+						</li>
+					</ul>
+				</AnimationSection>
+			</div>
+		</section>
+	);
+};
 
 export const Section = observer(SectionInternal);
