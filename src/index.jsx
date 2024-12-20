2dc4a03ef947200618c3d54721e85039e68bc5d3
fletchthefletch <fletcher@4tradeit.co.nz>
2024-12-20 15:18:18 +1300

    chore: removed unnecessary styles

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/css/fontawesome-all.min.css';
 import './assets/css/main.css';
-import './assets/css/noscript.css';
 import './assets/css/overrides.css';
 import App from './App.tsx';
 
