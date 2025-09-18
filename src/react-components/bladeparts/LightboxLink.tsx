c0bf026b73475bcf27b28b9fed861f90e4862a28
fletchthefletch <fletcher@4tradeit.co.nz>
2025-09-18 18:11:39 +1200

    Add basic modal draft

--- src/react-components/bladeparts/LightboxLink.tsx
+++ src/react-components/bladeparts/LightboxLink.tsx
@@ -9,29 +9,6 @@ interface Props {
 	text: string;
 }
 
-const customStyles = {
-	// overlay: {
-	// 	position: 'fixed',
-	// 	top: 0,
-	// 	left: 0,
-	// 	right: 0,
-	// 	bottom: 0,
-	// 	backgroundColor: 'rgba(0, 0, 0, 0.5)',
-	// },
-	// content: {
-	// 	top: '50%',
-	// 	left: '50%',
-	// 	right: 'auto',
-	// 	bottom: 'auto',
-	// 	marginRight: '-50%',
-	// 	transform: 'translate(-50%, -50%)',
-	// 	backgroundColor: 'rgba(255, 255, 255, 1)',
-	// 	boxShadow: '0 12px 15px 0 rgba(0,0,0,.24)',
-	// 	width: '80%',
-	// 	height: '80%',
-	// },
-};
-
 const LightboxLinkInternal: FC<Props> = ({ link, text }) => {
 	const [isModalOpen, toggleModal] = useToggle();
 
@@ -48,18 +25,31 @@ const LightboxLinkInternal: FC<Props> = ({ link, text }) => {
 				// contentLabel='Example Modal'
 				overlayClassName='overlay'
 			>
-				{/* <button onClick={toggleModal}>close</button> */}
-				<iframe
-					src='https://docs.google.com/forms/d/e/1FAIpQLSciIByP3-EO_Ucv5W2UDLiNEnf4VjVwJANrv6yYPf3-z0E4kQ/viewform?embedded=true'
-					width='100%'
-					height='100%'
-					frameborder='0'
-					marginheight='0'
-					marginwidth='0'
-					loading='lazy'
-				>
-					<FadeLoader color='#003366' />
-				</iframe>
+				<div className='modal-content'>
+					<div className='modal-actions'>
+						<a
+							href={link}
+							target='_blank'
+							className='button modal-button'
+							rel='noreferrer'
+						>
+							{'Open Form in New Tab'}
+						</a>
+						<button onClick={toggleModal}>close</button>
+					</div>
+					<iframe
+						src='https://docs.google.com/forms/d/e/1FAIpQLSciIByP3-EO_Ucv5W2UDLiNEnf4VjVwJANrv6yYPf3-z0E4kQ/viewform?embedded=true'
+						width='100%'
+						height='100%'
+						frameBorder='0'
+						marginHeight={0}
+						marginWidth={0}
+						loading='lazy'
+						title='Sign Up Form'
+					>
+						<FadeLoader color='#003366' />
+					</iframe>
+				</div>
 			</Modal>
 		</>
 	);
